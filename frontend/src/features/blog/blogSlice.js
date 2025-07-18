b9049ac5aa3384d7c4f00590b78456461d0fad4c
SatyamDevGenie <satyamsawant54@gmail.com>
2025-07-18 07:11:21 +0530

    again pasted deployed link

--- frontend/src/features/blog/blogSlice.js
+++ frontend/src/features/blog/blogSlice.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 // Base API URL
-const BLOG_API_URL = "http://localhost:5000/api/blogs";
+const BLOG_API_URL = "https://blogsy-yttu.onrender.com/api/blogs";
 
 // =======================
 // Async Thunks
